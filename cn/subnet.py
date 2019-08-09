faf3938eb5039a9c4181a6ade97589cf849117df
ksdfg <ksdfg123@gmail.com>
2019-08-09 22:55:05 +0530

    removed random ` from ddl.txt

--- cn/subnet.py
+++ cn/subnet.py
@@ -10,9 +10,24 @@ elif 224 <= ip[0] < 240:
     ip_class = 'D'
 else:
     ip_class = 'E'
-    print('Class E IP, this shizz useless :(')
+    print('class E ip, this shizz useless :(')
     exit(1)
 
 print(
     'The class of ip address {} is {}'.format('.'.join(map(str, ip)), ip_class)
 )
+
+default_masks = {
+    'A': '255.0.0.0',
+    'B': '255.255.0.0',
+    'C': '255.255.255.0',
+    'D': '255.255.255.255'
+}
+
+number_hosts = int(input("Enter number of hosts: "))
+i = 1
+while True:
+    result = 2 ** i
+    if number_hosts <= result:
+        break
+    i += 1
