3d4dd7dbd1ba1611436a6383754ded6767c9154c
ksdfg <ksdfg123@gmail.com>
2019-08-14 12:14:02 +0530

    added comments to subnet

--- cn/subnet.py
+++ cn/subnet.py
@@ -1,5 +1,7 @@
 import re
 
+# index 0 - part of the subnet mask that won't change
+# index 1 - no. of host bits
 masks = {
     'A': ['255.', 24],
     'B': ['255.255.', 16],
@@ -7,6 +9,7 @@ masks = {
 }
 
 
+# converting the binary part of ip address to decimals seperated by bytes
 def convert(binary):
     decimal = []
     for i in range(0, len(binary), 8):
@@ -18,6 +21,7 @@ ip = list(map(int, input('Enter ip : ').split('.')))
 for_subnet = ''
 ip_class = ''
 
+# for_subnet is basically what will bytes will stay same when you make subnets
 if ip[0] <= 126:
     ip_class = 'A'
     for_subnet = '.'.join(map(str, ip[:1])) + '.'
@@ -35,22 +39,22 @@ print(
     '\nThe class of ip address {} is {}\n'.format('.'.join(map(str, ip)), ip_class)
 )
 
-number_hosts = int(input("Enter number of hosts: ")) + 2
-i = 1
+ip_range = int(input("Enter number of hosts: ")) + 2  # +2 cuz first and last address not available
+i = 1  # 2**i will be the closest to ip_range
 while True:
     result = 2 ** i
-    if number_hosts <= result:
+    if ip_range <= result:
         break
     i += 1
 
-mask = ('0' * i).rjust(masks[ip_class][1], '1')
+mask = ('0' * i).rjust(masks[ip_class][1], '1')  # part of the subnet mask after the part included in masks
 
-n = 2 ** (masks[ip_class][1] - i)
+n = 2 ** (masks[ip_class][1] - i)  # number of ip addresses in each subnet
 
 subnets = []
 for k in range(n):
-    start = re.sub('1+', str(bin(k)[2:]), mask).rjust(masks[ip_class][1], '0')
-    end = start[:-i] + '1' * i
+    start = re.sub('1+', str(bin(k)[2:]), mask).rjust(masks[ip_class][1], '0')  # basically replace 111... with a no.
+    end = start[:-i] + '1' * i  # replace all the 0s of the subnet mask with 1s to get the last address
     subnets.append([start, end])
 
 print('the subnet mask is', masks[ip_class][0] + convert(mask), '\n')
