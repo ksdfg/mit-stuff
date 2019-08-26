34c862cfebd60a837b5205d4163925fd0a4d1c82
Kshitish Deshpande <43273771+ksdfg@users.noreply.github.com>
2019-08-26 05:37:49 +0100

    Update pir.py

--- iot/pir.py
+++ iot/pir.py
@@ -7,22 +7,13 @@ GPIO.setwarnings(False)
 pir = 18
 led = 24
 
-print('led on')
-GPIO.setup(pir, GPIO.IN)
-time.sleep(1)
-GPIO.setup(led, GPIO.OUT)
-print('led off')
+GPIO.output(led,GPIO.HIGH)
 
-GPIO.output(led, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(led, GPIO.LOW)
-time.sleep(1)
-
-for i in range(15):
-    if GPIO.input(pir):
-        print('yay')
-        GPIO.output(led, GPIO.HIGH)
-        time.sleep(0.5)
-        GPIO.output(led, GPIO.LOW)
-        
-GPIO.cleanup()
+try:
+    while(1):
+        if GPIO.input(pir) != 1:
+            GPIO.output(led, GPIO.HIGH)
+        else:
+            GPIO.output(led, GPIO.LOW)
+except KeyboardInterrupt:
+    GPIO.cleanup()
