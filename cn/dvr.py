943bb7176404f71b43c631b7ad5c336aeddb847d
ksdfg <ksdfg123@gmail.com>
2019-09-11 15:38:33 +0530

    finished dvr, added ss for rip

--- cn/dvr.py
+++ cn/dvr.py
@@ -1,5 +1,53 @@
-import numpy as np
-from Algorithm import *
+class Algorithm(object):
+
+    @staticmethod
+    def shareRT(vertexList):
+
+        print("---------------------\n\nRouting Tables being transmitted 1 by 1:")
+        # UPDATE_HAPPENED = False
+
+        while True:
+            UPDATE_HAPPENED = False
+            for receiver in vertexList:
+                print("-----------------------------------------------")
+                for sender in receiver.neighbours:
+                    current_cost = 0
+                    next_hop = '#'
+                    for lol in receiver.routingTable:
+                        if lol[0] == sender.name:
+                            current_cost = lol[1]
+                            next_hop = lol[2]
+                    print(sender.name, " sends its DV to ", receiver.name, " , and is at distance ", current_cost,
+                          " from it.")
+
+                    for i in receiver.routingTable:
+                        L1 = list(i)
+                        for j in sender.routingTable:
+                            L2 = list(j)
+                            if (i[0] == j[0]) & ((j[1] + current_cost) < i[1]):
+                                print(L1, "\t", " and ", end='')
+                                print(L2, " ", end='')
+                                L1[1] = L2[1] + current_cost
+                                L1[2] = next_hop  # changed from sender.name
+                                receiver.routingTable.remove((i[0], i[1], i[2]))
+                                i = tuple(L1)
+                                receiver.routingTable.append(i)
+                                print(" updates L1 to ", i)
+                                UPDATE_HAPPENED = True
+                            else:
+                                continue
+                        # i = tuple(L1)
+                if UPDATE_HAPPENED == True:
+                    print("\n\nUPDATED", receiver.name, "'s Routing Table:")
+                    receiver.routingTable.sort()
+                    for j in receiver.routingTable:
+                        print(j, "\t", end='')
+                    print()
+
+            if (UPDATE_HAPPENED == False):
+                break
+
+        print("STABLE STATE REACHED!")
 
 
 class Node(object):
@@ -55,13 +103,13 @@ for i in range(m):
 
     cost = int(input())
 
-    for i in vertexList:
+    for j in vertexList:
 
-        if i.name == sourceLabel.name:
-            sourceLabel = i
+        if j.name == sourceLabel.name:
+            sourceLabel = j
 
-        if i.name == destLabel.name:
-            destLabel = i
+        if j.name == destLabel.name:
+            destLabel = j
 
     for lol in sourceLabel.routingTable:
 
@@ -99,49 +147,15 @@ algorithm = Algorithm()
 
 algorithm.shareRT(vertexList)
 
-while True:
+print("-----------------FINAL------------------------")
 
-    print("Update cost of some link? Y:N")
+for i in vertexList:
 
-    opt = input()
+    print(i.name, "'s Routing Table:")
 
-    if opt == 'Y':
+    i.routingTable.sort()
 
-        print("Source Name, Destination Name, Cost:")
+    for j in i.routingTable:
+        print(j, "\t", end='')
 
-        sourceLabel = Node(input(), 0)
-
-        destLabel = Node(input(), 0)  # don't make another routing table, since will be replaced
-
-        cost = int(input())
-
-        earlier_cost = 0
-
-        for i in vertexList:
-
-            if i.name == sourceLabel.name:
-                sourceLabel = i
-
-            if i.name == destLabel.name:
-                destLabel = i
-
-        for lol in sourceLabel.routingTable:
-
-            if lol[0] == destLabel.name:
-                earlier_cost = lol[1]
-
-        print("Earlier cost from ", sourceLabel.name, " to ", destLabel.name, " was ", earlier_cost)
-
-        sourceLabel.routingTable.remove((destLabel.name, earlier_cost, destLabel.name))
-
-        sourceLabel.routingTable.append((destLabel.name, cost, destLabel.name))
-
-        destLabel.routingTable.remove((sourceLabel.name, earlier_cost, sourceLabel.name))
-
-        destLabel.routingTable.append((sourceLabel.name, cost, sourceLabel.name))
-
-        algorithm.shareRT(vertexList)
-
-    else:
-
-        break
+    print()
