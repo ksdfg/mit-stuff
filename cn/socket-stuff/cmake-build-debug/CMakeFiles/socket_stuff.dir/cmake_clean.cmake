7d7654f0e3bad5bcf940c454bc5c7b3b91ace33a
ksdfg <ksdfg123@gmail.com>
2019-09-14 22:25:27 +0530

    still need to write the actual code for socket...

---
+++ cn/socket-stuff/cmake-build-debug/CMakeFiles/socket_stuff.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/socket_stuff.dir/main.cpp.o"
+  "socket_stuff.pdb"
+  "socket_stuff.exe"
+  "libsocket_stuff.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/socket_stuff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cn/socket-stuff/cmake-build-debug/CMakeFiles/socket_stuff.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for socket_stuff.
+# This may be replaced when dependencies are built.
