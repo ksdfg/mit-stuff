2fddf19a9f638bcd81d616e19dbd2bcc14a2c544
ksdfg <ksdfg123@gmail.com>
2019-11-26 23:03:41 +0530

    fixed bubble sort

--- hpc/bubble.c
+++ hpc/bubble.c
@@ -18,10 +18,10 @@ int main(){
 	double start = omp_get_wtime();	// record start time
 
 	// use bubble sort
-	for(int i=0; i<5; i++){
+	for(int i=0; i<size; i++){
 
 		#pragma omp for
-		for(int j=0; j<4; j+=2){	// compare even positioned elements
+		for(int j=0; j<size-1; j+=2){	// compare even positioned elements
 			if(arr[j]>arr[j+1]){
 				// swap
 				temp = arr[j];
@@ -31,7 +31,7 @@ int main(){
 		}
 
 		#pragma omp for
-                for(int j=1; j<4; j+=2){        // compare odd positioned elements
+                for(int j=1; j<size-1; j+=2){        // compare odd positioned elements
                         if(arr[j]>arr[j+1]){
 				// swap
                                 temp = arr[j];
