83a81b735f4b321052a92a60c468b7f5a7d5e756
ksdfg <ksdfg123@gmail.com>
2020-01-09 14:53:58 +0530

    small change in is 1st episode

--- is/main.py
+++ is/main.py
@@ -5,5 +5,5 @@ set_key()
 string = input('Enter string to encode : ')
 print(
     'The encoded string is : ', encode(string),
-    '\nThe decoded string is : ', decode(shuffle.encode(string))
+    '\nThe decoded string is : ', decode(encode(string))
 )
