ad835fab0b944c9e187a0eb0ce5b2b37c8683257
ksdfg <ksdfg123@gmail.com>
2020-07-28 22:01:15 +0530

    ssc: kang aditya's code and format it
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- t9/ssc/src/Assigment1.java
+++ t9/ssc/src/Assigment1.java
@@ -1,6 +1,4 @@
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 class Operands {
@@ -109,20 +107,143 @@ public class Assigment1 {
         return address;
     }
 
+    public static String stringOfTuple(Operands tuple) {
+        return "(" + tuple.getStatement_class() + "," + tuple.getMachine_code() + ") ";
+    }
+
     public static void main(String[] args) {
-        Map<String, Operands> OPTAB = (new Operands()).getOpTable();
+        Map<String, Operands> OpTable = (new Operands()).getOpTable();
+
         LinkedHashMap<String, TableValue> symbol_table = new LinkedHashMap<>();
         LinkedHashMap<String, TableValue> literal_table = new LinkedHashMap<>();
         List<Integer> pool_table = new ArrayList<>();
 
+        int symbol_table_pointer = 1;
+        int literal_table_pointer = 1;
+        int address_pointer = 0;
+        int tokenCounter;
+
+        ArrayList<String> intermediateCode = new ArrayList<>();
+        StringBuilder intermediateLine = new StringBuilder();
+
         try {
-            // Read code from file
             Scanner scanner = new Scanner(new FileReader("/home/ksdfg/stuff/code/mit-stuff/t9/ssc/src/code.txt"));
 
             while (scanner.hasNextLine()) {
-                // TODO : Need to go through everything here
+                String[] tokens = scanner.nextLine().split(" ");
+
+                if (address_pointer != 0) {
+                    intermediateLine.append(address_pointer).append(") ");
+                }
+
+                tokenCounter = 0;
+                for (int i = 0; i < tokens.length; i++) {
+                    Operands opTuple;
+                    String token = tokens[i];
+
+                    if (isOpCode(token)) {
+                        opTuple = OpTable.get(token);
+                        switch (token) {
+                            case "END" -> {
+                                intermediateLine = new StringBuilder();
+                                intermediateLine.append(stringOfTuple(OpTable.get(token)));
+                                address_pointer = setLiteralTable(address_pointer, literal_table);
+                                pool_table.add(literal_table_pointer);
+                            }
+                            case "LTORG" -> {
+                                intermediateLine = new StringBuilder();
+                                address_pointer = setLiteralTable(address_pointer, literal_table);
+                                pool_table.add(literal_table_pointer);
+                            }
+                            case "START" -> {
+                                address_pointer = Integer.parseInt(tokens[i + 1]) - 1;
+                                intermediateLine.append("(C,").append(Integer.parseInt(tokens[i + 1])).append(") ");
+                                i++;
+                            }
+                            case "ORIGIN" -> {
+                                TableValue symbol_table_item = symbol_table.get(tokens[i + 1]);
+                                address_pointer = symbol_table_item.getAddress();
+                                i++;
+                            }
+                        }
+
+                        if (opTuple.getStatement_class().equals("RG") || opTuple.getStatement_class().equals("CC")) {
+                            intermediateLine.append("(").append(opTuple.getMachine_code()).append(") ");
+                        } else {
+                            intermediateLine.append(stringOfTuple(opTuple));
+                        }
+                    } else if (symbol_table.containsKey(token)) {
+                        intermediateLine.append("(S,").append(symbol_table.get(token).getIndex()).append(") ");
+                    } else if (tokenCounter == 0) {
+                        TableValue symbol_table_item = new TableValue();
+                        symbol_table_item.setAddress(address_pointer);
+                        symbol_table_item.setIndex(symbol_table_pointer);
+                        symbol_table.put(token, symbol_table_item);
+                        symbol_table_pointer++;
+                    } else if (isNumeric(token)) {
+                        if (tokens[i - 1].equals("DS")) {
+                            TableValue symbol_table_item = new TableValue();
+                            symbol_table_item.setAddress(address_pointer);
+                            symbol_table_item.setIndex(symbol_table.get(tokens[i - 2]).getIndex());
+                            symbol_table.put(tokens[i - 2], symbol_table_item);
+                        } else if (tokens[i - 1].equals("DC")) {
+                            TableValue symbol_table_item = new TableValue();
+                            symbol_table_item.setAddress(address_pointer);
+                            symbol_table_item.setIndex(symbol_table.get(tokens[i - 2]).getIndex());
+                            symbol_table.put(tokens[i - 2], symbol_table_item);
+                        }
+                        intermediateLine.append("(C,").append(token).append(") ");
+                    } else {
+                        if (token.startsWith("=")) {
+                            TableValue littabHolder = new TableValue();
+                            littabHolder.setIndex(literal_table_pointer);
+                            intermediateLine.append("(L,").append(literal_table_pointer).append(") ");
+                            literal_table_pointer++;
+                            literal_table.put(token, littabHolder);
+                        } else {
+                            TableValue symbol_table_item = new TableValue();
+                            symbol_table_item.setAddress(-1);
+                            symbol_table_item.setIndex(symbol_table_pointer);
+                            symbol_table.put(token, symbol_table_item);
+                            intermediateLine.append("(S,").append(String.valueOf(symbol_table_pointer)).append(") ");
+                            symbol_table_pointer++;
+                        }
+                    }
+                    tokenCounter++;
+                }
+
+                address_pointer++;
+                intermediateCode.add(intermediateLine.toString());
+                intermediateLine = new StringBuilder();
             }
         } catch (IOException e) {
+            System.out.println("Input file not found");
+            e.printStackTrace();
+        }
+
+        try {
+            File out = new File("/home/ksdfg/stuff/code/mit-stuff/t9/ssc/src/output.txt");
+            FileOutputStream fos = new FileOutputStream(out);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+
+            for (String line : intermediateCode) {
+                bw.write(line + "\n");
+            }
+
+            bw.write("\nSYMBOL TABLE: \n");
+            printTable(bw, symbol_table);
+
+            bw.write("\nLITERAL TABLE: \n");
+            printTable(bw, literal_table);
+
+            bw.write("\npool_table:\n");
+            for (int i : pool_table) {
+                bw.write(i + "\n");
+            }
+
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("Output file not found");
             e.printStackTrace();
         }
     }
