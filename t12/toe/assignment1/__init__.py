3e5bd770124b0d5e9cc871580d017c9e7731abb8
ksdfg <ksdfg123@gmail.com>
2021-04-09 10:43:30 +0530

    t12: TOE: assignment1: add function to fetch links from google scholars

--- t12/toe/assignment1/__init__.py
+++ t12/toe/assignment1/__init__.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, List
 
 from decouple import config
 from selenium import webdriver
@@ -67,6 +67,56 @@ def run_github_login(email: str, password: str) -> str:
         driver.close()
         return username
 
+    except Exception as e:
+        driver.close()
+        raise e
+
+
+def run_google_scholar(query: str) -> List[str]:
+    """
+    Get 5 articles from Google Scholar that are published after 2020
+    :param query: Query string to be searched to get list of
+    :return: links to articles
+    """
+    options = Options()
+    options.headless = config("ass1_headless", cast=bool, default=False)
+    driver = webdriver.Firefox(options=options)
+
+    try:
+        # go to google scholars
+        driver.get("https://scholar.google.com/")
+
+        # define IDs for query input and search button
+        query_input_id = "gs_hdr_tsi"
+        search_button_id = "gs_hdr_tsb"
+
+        # wait for query input and search button to load, then enter query and search
+        _wait_for_elements(driver=driver, locators=((By.ID, query_input_id), (By.ID, search_button_id)))
+        driver.find_element_by_id(query_input_id).send_keys(query)
+        driver.find_element_by_id(search_button_id).click()
+
+        # define link text for link to articles since 2020
+        since_2020_link_text = "Since 2020"
+
+        # wait for link to load and click on it
+        _wait_for_elements(driver=driver, locators=((By.LINK_TEXT, since_2020_link_text),))
+        driver.find_element_by_link_text(since_2020_link_text).click()
+
+        # define class name for divs showing an article and xpath to anchor tag with article link from the article div as root
+        article_div_class = "gs_ri"
+        article_anchor_tag_xpath = "h3/a"
+
+        # wait for divs to load
+        _wait_for_elements(driver=driver, locators=((By.CLASS_NAME, article_div_class),))
+        article_divs = driver.find_elements_by_class_name(article_div_class)
+
+        # iterate through the first 5 article divs and get links
+        links = []
+        for article_div in article_divs[:5]:
+            links.append(article_div.find_element_by_xpath(article_anchor_tag_xpath).get_property("href"))
+
+        driver.close()
+        return links
     except Exception as e:
         driver.close()
         raise e
---
+++ t12/toe/assignment1/__main__.py
@@ -0,0 +1,4 @@
+from assignment1 import run_google_scholar
+
+if __name__ == "__main__":
+    print(*run_google_scholar("anime cats"), sep="\n")
